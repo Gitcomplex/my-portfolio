93c3a717d78f8e17d9a54b8d4a6f433a5e374c16
Gitcomplex <85607452+Gitcomplex@users.noreply.github.com>
2024-11-20 09:10:02 +0000

    added links for the social media platforms

--- app/config.ts
+++ app/config.ts
@@ -8,9 +8,9 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com/dipansh72820618",
+  github: "https://github.com/Gitcomplex",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/yadavdipanshu",
+  email: "mailto:dipaklucnow33@gmail.com",
 };
